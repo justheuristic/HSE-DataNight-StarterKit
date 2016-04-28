dc12810787c2fd27d852a4eb88b21fbc2e476237
trakhovroma <trakhovroma@gmail.com>
2016-04-28 03:04:49 +0300

    tsk6

---
+++ tests/test_of_test.py
@@ -0,0 +1,14 @@
+import unittest
+import open_check
+
+class TestMyTest(unittest.TestCase):
+
+  def test_test(self):
+    self.assertTrue(test.test())
+
+if __name__ == '__main__':
+    unittest.main()
+
+    Status API Training Shop Blog About 
+
+
